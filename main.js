ae7bba9752c20ef60e594bd8a41c3abe57313b35
Jack Nortje <jacknortje24@gmail.com>
2021-08-27 09:34:26 +0200

    Added color and naming

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let container = document.getElementById('div-container');
-let button = document.querySelector('button')
+let button = document.getElementById('restartbtn')
+const buttonColor = document.getElementById('colorbtn')
+
 
 for (let i = 0; i < 256; i++) {
     const arbitraryDiv = document.createElement('div');
@@ -10,12 +12,30 @@ for (let i = 0; i < 256; i++) {
     })
     button.addEventListener("click", function() {
         arbitraryDiv.style.backgroundColor = 'black';
-        
+        arbitraryDiv.addEventListener("mouseover", function() {
+            arbitraryDiv.style.backgroundColor = 'white'
+        })
+      })
+      buttonColor.addEventListener("click", function() {
+        arbitraryDiv.style.backgroundColor = 'black';
+        arbitraryDiv.addEventListener("mouseover", function() {
+            arbitraryDiv.style.backgroundColor = getRandomColor()
+        })
       })
     container.append(arbitraryDiv)
     
 
-}
+};
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 
 
 
